b0af83d9d06b31794082b647d0b2dc4762520099
Kumar <kumarsumit1989@gmail.com>
2023-05-06 06:30:26 +0530

    Added cors and morgan logger middlewares

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // eslint-disable-next-line strict
 "use strict";
 // All imports go here
+// Package imports
 import express from "express";
 import dotenv from "dotenv";
 // eslint-disable-next-line no-unused-vars
 import colors from "colors";
+import cors from "cors";
+import morgan from "morgan";
+// File imports
 import connectDB from "./config/db.js";
 
 // config dotenv
@@ -15,9 +19,14 @@ connectDB();
 
 // express app object
 const app = express();
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
 // All routes go here
 
+// Error handling middleware goes here after the routes
 // GET PORT
 const PORT = process.env.PORT || 8080;
 // listening to server
